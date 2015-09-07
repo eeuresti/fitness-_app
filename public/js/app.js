735865cbc2cca0afea69bbc801d6675b58371214
Melissa M. Dailey <mel.dailey@gmail.com>
2015-09-06 23:51:38 -0700

    testing modal and carousel

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,64 @@
-
 //========================================
 // <<<<<<<<<< Start Countdown >>>>>>>>>>>>
 //========================================
 
-var redirect = "/workout1";
-
-function startTimer(duration, display) {
-    var timer = duration, seconds;
-    setInterval(function () {
-        //minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        //minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = seconds;
-
-        if (--timer < 0) {
-            window.location.href = redirect;
-        }
-    }, 1000);
-}
-
-window.onload = function () {
-    var tenSeconds = 10,
-        display = document.querySelector('#time');
-    startTimer(tenSeconds, display);
-};
 
 
+// function startTimer(duration, display) {
+//     var timer = duration, seconds;
+//     setInterval(function () {
+//         //minutes = parseInt(timer / 60, 10)
+//         seconds = parseInt(timer % 60, 10);
 
-// Activate Carousel
-$("#myCarousel").carousel();
+//         //minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-// Enable Carousel Indicators
-$(".item").click(function(){
-    $("#myCarousel").carousel(1);
+//         display.textContent = seconds;
+
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+// }
+
+// window.onload = function () {
+//     var thirtySeconds = 3,
+//         display = document.querySelector('#thirtySeconds');
+//     startTimer(thirtySeconds, display);
+// };
+
+$( document ).ready(function() {
+
+
+$('#myCarousel').bind('slide.bs.carousel', function (e) {
+    console.log('slide event!');
 });
 
-// Enable Carousel Controls
-$(".left").click(function(){
-    $("#myCarousel").carousel("prev");
+$('#myCarousel').on('slide.bs.carousel', function (e) {
+    console.log('slide ON event!');
+    $('#myModal').modal('show'); 
 });
 
 
+});
 
+// $("#carousel-example-generic").on('slide.bs.carousel', function () {
+//             console.log('A new slide is about to be shown!');
+//             // $('#myModal').modal('show'); 
+//     });
+
+
+
+// // Activate Carousel
+// $("#myCarousel").carousel();
+
+// // Enable Carousel Indicators
+// $(".item").click(function(){
+//     $("#myCarousel").carousel(1);
+// });
+
+// // Enable Carousel Controls
+// $(".left").click(function(){
+//     $("#myCarousel").carousel("prev");
+// });
 
