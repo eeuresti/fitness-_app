db1ae4b4112e835bdf13ab9ac608578f71121ad3
Melissa M. Dailey <mel.dailey@gmail.com>
2015-09-07 16:13:00 -0700

    workout & break intervals completed with redirect to menu page upon completion

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 //========================================
-// <<<<<<<<<< Main Countdown >>>>>>>>>>>>
+// <<<<<<<<<< Routine Countdown >>>>>>>>>>
 //========================================
 
 
@@ -10,37 +10,111 @@ $(document).ready(function() {
     var secondsLeft = 0;
     var countDown = setInterval(function(){
       
-      console.log(counter);
       counter--; //decrease the counter each second
       secondsLeft--; //decrease time left in current excersice 
+      var workSession = 30;
+      var breakSession = 10;
+      var redirect = "/menu";
+
+      $(".countdown").html("Just " + secondsLeft + " seconds to go!");
 
         if (counter === 479) {
-            $(".routine").append("<p class='itemName'>Let's gets started!</p>", "<div>Workout starts in <span id='time'>10</span> seconds!</div>", tenSeconds());
+            $(".routine").append("<p class='itemName'>Let's gets started!</p>");
+            secondsLeft = breakSession;
         }
         else if (counter === 470) {
             $(".routine").empty().append("<p class='itemName'>Jumping Jacks!</p>");
+            secondsLeft = workSession;
         }
         else if (counter === 440) {
             $(".routine").empty().append("<p class='itemName'>Take a break!</p>");
+            secondsLeft = breakSession;
         }
         else if (counter === 430) {
             $(".routine").empty().append("<p class='itemName'>Lounges!</p>");
+            secondsLeft = workSession;
         }
         else if (counter === 400) {
             $(".routine").empty().append("<p class='itemName'>Take a Break!</p>");
+            secondsLeft = breakSession;
         }
         else if (counter === 390) {
             $(".routine").empty().append("<p class='itemName'>Sit Ups!</p>");
+            secondsLeft = workSession;
         }
         else if (counter === 360) {
             $(".routine").empty().append("<p class='itemName'>Take break!</p>");
+            secondsLeft = breakSession;
         }
         else if (counter === 350) {
             $(".routine").empty().append("<p class='itemName'>Plank!</p>");
+            secondsLeft = workSession;
         }
-        else if (counter === 0) {
+         else if (counter === 320) {
+            $(".routine").empty().append("<p class='itemName'>Take break!</p>");
+            secondsLeft = breakSession;
+        }
+        else if (counter === 310) {
+            $(".routine").empty().append("<p class='itemName'>Plank!</p>");
+            secondsLeft = workSession;
+        }
+        else if (counter === 280) {
+            $(".routine").empty().append("<p class='itemName'>Take break!</p>");
+            secondsLeft = breakSession;
+        }
+        else if (counter === 270) {
+            $(".routine").empty().append("<p class='itemName'>Plank!</p>");
+            secondsLeft = workSession;
+        }
+        else if (counter === 220) {
+            $(".routine").empty().append("<p class='itemName'>Take break!</p>");
+            secondsLeft = breakSession;
+        }
+        else if (counter === 210) {
+            $(".routine").empty().append("<p class='itemName'>Plank!</p>");
+            secondsLeft = workSession;
+        }
+        else if (counter === 170) {
+            $(".routine").empty().append("<p class='itemName'>Take break!</p>");
+            secondsLeft = breakSession;
+        }
+        else if (counter === 160) {
+            $(".routine").empty().append("<p class='itemName'>Plank!</p>");
+            secondsLeft = workSession;
+        }
+        else if (counter === 130) {
+            $(".routine").empty().append("<p class='itemName'>Take break!</p>");
+            secondsLeft = breakSession;
+        }
+        else if (counter === 120) {
+            $(".routine").empty().append("<p class='itemName'>Plank!</p>");
+            secondsLeft = workSession;
+        }
+        else if (counter === 90) {
+            $(".routine").empty().append("<p class='itemName'>Take break!</p>");
+            secondsLeft = breakSession;
+        }
+        else if (counter === 80) {
+            $(".routine").empty().append("<p class='itemName'>Plank!</p>");
+            secondsLeft = workSession;
+        }
+        else if (counter === 50) {
+            $(".routine").empty().append("<p class='itemName'>Take break!</p>");
+            secondsLeft = breakSession;
+        }
+        else if (counter === 40) {
+            $(".routine").empty().append("<p class='itemName'>Plank!</p>");
+            secondsLeft = workSession;
+        }
+        else if (counter === 10) {
+            $(".routine").empty().append("<p class='itemName'>You are Done! Great Workout!!</p>");
+            secondsLeft = breakSession;
+        }
+        else if (counter === 5) {
             console.log("Finished!!");
-            clearInterval(countDown);
+          }
+          else if (counter === 0) {
+            window.location.href = redirect;
           }
     }, 1000); 
     
@@ -50,84 +124,8 @@ $(document).ready(function() {
 
 
 
-//========================================
-// <<<<<<<<<< Countdowns >>>>>>>>>>>>
-//========================================
-
-// function countDown(s) {
-//     var counter = s;
-//     var countDown = setInterval(function(){
-//       console.log(counter);
-//       counter--
-//       if (counter === 0) {
-//         console.log("Finished!!");
-//         clearInterval(countDown);
-//       }
-//     }, 1000);  
-
-// };
 
 
-
-
-// //test countdown
-
-// function startTimer(duration, display) {
-//     var timer = duration, seconds;
-//     setInterval(function () {
-//         //minutes = parseInt(timer / 60, 10)
-//         seconds = parseInt(timer % 60, 10);
-
-//         //minutes = minutes < 10 ? "0" + minutes : minutes;
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
-//         display.textContent = seconds;
-
-//         if (--timer < 0) {
-//             timer = duration;
-//         }
-//     }, 1000);
-// };
-
-
-// function timing(s) {
-//     var seconds = s,
-//     display = document.querySelector('#timer');
-//     startTimer(seconds, display);
-// } ;
-
-
-
-
-
-
-
-//========================================
-// <<<<<<<<<< Start Countdown >>>>>>>>>>>>
-//========================================
-
-function startTimer(duration, display) {
-    var timer = duration, seconds;
-    setInterval(function () {
-        //minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        //minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = seconds;
-        console.log(seconds, "text content seconds");
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-function tenSeconds() {
-    var seconds = 10;
-    display = document.querySelector('#time');
-    startTimer(seconds, display);
-};
 
 
 
