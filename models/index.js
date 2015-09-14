4fcdf72132ef17efe33d6fb784d91bde24856b7f
Eric Euresti <eeuresti@gmail.com>
2015-09-14 09:44:09 -0700

    fixed syntax index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
     mongoose.connect( process.env.MONGOLAB_URI ||
-                      process.env.MONGOHQ_URL || 
-                      "mongodb://localhost/express_auth")
+                      process.env.MONGOHQ_URL ||
+                      "mongodb://localhost/express_auth");
     module.exports.User = require("./user");
